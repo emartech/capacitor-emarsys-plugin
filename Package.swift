d7dc917a52256568e5049115acf0527f86d40ecb
Eduardo Zatoni <45048607+eduzatoni@users.noreply.github.com>
2026-08-28 16:09:01 +0200

    chore: add emarsys sdk to SPM

--- Package.swift
+++ Package.swift
@@ -3,21 +3,23 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorEmarsysPlugin",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapacitorEmarsysPlugin",
             targets: ["EmarsysPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
+        .package(url: "https://github.com/emartech/ios-emarsys-sdk.git", from: "3.10.2")
     ],
     targets: [
         .target(
             name: "EmarsysPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "EmarsysSDKLibrary", package: "ios-emarsys-sdk")
             ],
             path: "ios/Sources/EmarsysPlugin"),
         .testTarget(
